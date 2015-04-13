203bcfcb9ddd47be6eb844e14627aeed2466d306
nikitasmall <nikitasosnov@yahoo.com>
2015-04-13 12:43:35 +0300

    basic http auth for admin controller

--- app/controllers/admin/statistic_controller.rb
+++ app/controllers/admin/statistic_controller.rb
@@ -1,9 +1,18 @@
 class Admin::StatisticController < ApplicationController
   layout "admin"
 
+  before_filter :authenticate
+
   # get /admin/statistic
   def index
 
     @question_rating = Question.select('user_id, sum(rating) as rating').group(:user_id)
+  end
+
+  protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'nikitasmall' && password == '2371757350'
+    end
   end
 end
