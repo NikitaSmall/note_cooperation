c1067f3a0cfdbe8c7708941eadf927f721b4a4f1
nikitasmall <nikitasosnov@yahoo.com>
2015-04-19 10:19:10 +0300

    added tag cloud in admin layout

--- app/controllers/concerns/current_tag.rb
+++ app/controllers/concerns/current_tag.rb
@@ -1,11 +1,6 @@
 module CurrentTag extend ActiveSupport::Concern
   protected
   def get_current_tag_name
-    tags = Tag.all
-    tag_names = Array.new
-    tags.each do |tag|
-      tag_names << tag.name
-    end
-    tag_names
+
   end
 end
